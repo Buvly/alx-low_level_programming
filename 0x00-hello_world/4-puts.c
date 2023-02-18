270a7156a4a8008f69bb61da065fc080fa89d87c
buvly <buvly.w@gmail.com>
2023-02-17 17:13:59 -0800

    I commit ;)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * this function for printing using puts
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
