bccf38e4455dd77e9f623d68cd742a19ad7b5e4f
buvly <buvly.w@gmail.com>
2023-02-18 11:47:36 -0800

    I commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -27,4 +25,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
