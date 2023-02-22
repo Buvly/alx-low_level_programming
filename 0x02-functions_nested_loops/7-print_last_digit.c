14361543eb810a3555979ff8f12ad90149e7f7b1
buvly <buvly.w@gmail.com>
2023-02-22 10:15:35 -0800

    Commiting

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 
 /**
  * print_last_digit - function prints last digit of a number.
- * @n: is a single input number 
- * Return: last digit in number n
+ * @n: is a single input number.
+ * Return: last digit in number n.
  */
 int print_last_digit(int n)
 {
 	int l;
-	
 	l = n % 10;
+	
 	if (l < 0)
 	{
 		l = -l;
