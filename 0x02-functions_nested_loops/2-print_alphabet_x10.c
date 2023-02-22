0c0e7fa02b7ff3efbb87b04d3afb9745db62c531
buvly <buvly.w@gmail.com>
2023-02-22 08:26:13 -0800

    Commiting

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0, j;
+	int i, j;
+	int i = 0;
 
 	while (i < 10)
 	{
-		for (j = 'a'; j <= 'z'; i++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
