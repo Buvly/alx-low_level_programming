b30bddd696c00bebc70b6ba32b950dbadcad1327
buvly <buvly.w@gmail.com>
2023-02-22 11:39:16 -0800

    Commiting

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add -  a function that prints the addition of 2 numbers
+ * @n: natural numbers
+ * Return: natural numbers from 0 to 98
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		pritnf("%d, ", n);
+	}
+	printf("\n");  
+}
+
