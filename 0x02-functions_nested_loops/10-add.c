80f453293e017a847928d51c3ec34a06b9870b80
buvly <buvly.w@gmail.com>
2023-02-22 11:30:38 -0800

    Commiting

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
 
 /**
  * add -  a function that prints the addition of 2 numbers
- * @a = first num
- * @ b = second, r = result
- * Return: summation of 2 numbers
+ * @a: first num
+ * @b: second
+ * Return: r summation of 2 numbers
  */
 int add(int a, int b)
 {
