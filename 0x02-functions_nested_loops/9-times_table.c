a067440abbade594d618df100271d0187234d807
buvly <buvly.w@gmail.com>
2023-02-22 10:59:41 -0800

    Commiting

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - function prints time table from 0 to 9.
+ * i = first numver, j = second number, result = result of multiplication.
+ * Return: time table.
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; m < 10; j++)
+		{
+			result = i * j;
+			_putchar(result);
+		}
+		_putchar('\n');
+	}
+}
+
