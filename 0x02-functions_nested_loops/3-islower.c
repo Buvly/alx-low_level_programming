4403d58670b9af3b9d71589ad2f2a2dc7514dac6
buvly <buvly.w@gmail.com>
2023-02-22 08:41:58 -0800

    Commiting

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+i#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - function prints only if  alphabet is in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c <= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
