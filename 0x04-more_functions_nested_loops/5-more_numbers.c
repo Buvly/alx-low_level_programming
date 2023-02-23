6d998eb9f4611ce8da5822398f4bb6032e4d852b
buvly <buvly.w@gmail.com>
2023-02-23 08:23:54 -0800

    I commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,15 @@ void more_numbers(void)
 {
 	int i, j;
 	
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');	
 	}
