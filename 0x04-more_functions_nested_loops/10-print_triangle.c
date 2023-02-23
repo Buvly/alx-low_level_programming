d2b265244f5c196dcd17203e6c85bf19e2a89567
buvly <buvly.w@gmail.com>
2023-02-23 11:22:41 -0800

    I commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_triangle -  draw a triangle 
+* @size: size of triangle (number of lines) 
+* Return: Null.
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
