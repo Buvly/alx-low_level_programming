fbf4086a1e6c5d9abb91b97452e53732e2801044
buvly <buvly.w@gmail.com>
2023-02-23 10:22:37 -0800

    I commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* main -  prints numbers from 1 to 100 and for multiples of 3 prints Fizz and multiples of 5 prints Buzz if both prints FizzBuzz.
+* Return: No return.
+*/
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n < 101; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf("\n");
+}
+
