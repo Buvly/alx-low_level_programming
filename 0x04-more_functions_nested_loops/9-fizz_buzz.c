03fb482b818cf05fb4cf578df875a865839fcc2e
buvly <buvly.w@gmail.com>
2023-02-23 10:40:41 -0800

    I commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 
 	n = 1;
 	printf("%d", n);
-	for (n = 2; n < 101; n++)
+	for (n = 2; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if ((n % 3 == 0) && (n % 5 == 0))	
 		{
 			printf(" FizzBuzz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf(" Buzz");
 		}
 		else if (n % 3 == 0)
 		{
 			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
 		}
 		else
 		{
@@ -33,4 +33,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
