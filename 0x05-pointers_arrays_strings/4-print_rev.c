bf83bfd04f26b9c9ed817cf24b732405fccd3bfd
buvly <buvly.w@gmail.com>
2023-02-27 16:06:39 -0800

    I commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse.
+ * @s:input string
+ * Return: NULL
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		count++;
+	}
+	for (count--; count >=0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
