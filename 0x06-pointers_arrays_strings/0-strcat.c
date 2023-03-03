324e579dfc1361e632652162b5e947ec48c6005d
buvly <buvly.w@gmail.com>
2023-03-02 22:43:59 -0800

    Committing

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
- */
+* _strcat - concatenates two strings,
+* @dest: destination.
+* @src: source.
+* Return: the pointer to dest.
+*/
 char *_strcat(char *dest, char *src)
 {
 	int count, count2;
