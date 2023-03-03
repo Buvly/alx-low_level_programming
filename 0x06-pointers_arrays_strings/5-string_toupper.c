1073aa0d1c30a3463b91c5fa3b010c062a4bdab2
buvly <buvly.w@gmail.com>
2023-03-03 08:30:07 -0800

    Committing

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - change all lower case to upper.
+ * @s: input string.
+ * Return: the pointer to string.
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+	  	{
+			*(s + count) = (s + count) - 32;
+		}
+		count++;
+	}
+	return (s);
+}	
