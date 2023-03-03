87a18cd9ffaf86ec1da5cbe20c8a4708b21a1a8c
buvly <buvly.w@gmail.com>
2023-03-03 06:59:22 -0800

    Committing

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,16 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int t;
-	
-	for (i = 0; i < n-1; i++)
+	int j;
+	int temp;
+
+	for (i = 0; i < n - 1; i++)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
