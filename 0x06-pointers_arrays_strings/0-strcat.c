b0d2740e029f4e74c13badee4f09e463673539c2
buvly <buvly.w@gmail.com>
2023-03-03 04:07:24 -0800

    Committing

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - concat 2 strings,
@@ -8,23 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int c1, c2;
-	c1 = 0;
-	c2 = 0;
+	int i, j;
 
-	while (*(dest + c1) != '\0')
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
 	{
-		c1++;
+		i++;
 	}
-	while (c2 >= 0)
+	while (src[j] != '\0')
 	{
-		*(dest + c1) = *(src + c2);
-		if (*(src + c2) == '\0')
-		{
-			break;
-		}
-		c1++;
-		c2++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
-}	
+}
