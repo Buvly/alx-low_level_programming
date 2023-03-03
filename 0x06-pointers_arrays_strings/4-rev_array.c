853556b0d727c6d63f79244e411b0f0cc5482fd8
buvly <buvly.w@gmail.com>
2023-03-03 06:19:08 -0800

    Committing

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse integers of an array.
+ * @a: array.
+ * @n: number of elements in array.
+ * Return: NULL.
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp
+	
+	for (i = 0; i < n-1; i++)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = t;
+	}
+}
