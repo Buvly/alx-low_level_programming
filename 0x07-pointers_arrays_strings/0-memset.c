2f17e670436efad1587a9035f5bed547cd4f33a7
buvly <buvly.w@gmail.com>
2023-03-06 14:00:40 -0800

    Commiting

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - function fills the first n bytes of the memory
+ * @*s: The pointer string
+ * @b : char to be printed
+ * @n : number of memory
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
