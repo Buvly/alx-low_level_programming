6757bda1b4e00d258e8f8dcfad3de12b62d860db
buvly <buvly.w@gmail.com>
2023-03-08 09:38:57 -0800

    Commiting

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - calc the fact.
+ * @n: integer
+ * Return: factorial.
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1))
+	}
+}
