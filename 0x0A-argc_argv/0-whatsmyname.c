104676678ab6402fec888dcf88ff8ba7699c21f1
buvly <buvly.w@gmail.com>
2023-03-10 17:17:22 -0800

    I commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array that contains the command line arguments.
  * Return: 0 in case of success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
