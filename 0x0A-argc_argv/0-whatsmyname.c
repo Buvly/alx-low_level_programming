3d1cfa8e35bcb1f40e882c48fe2ce8f417391329
buvly <buvly.w@gmail.com>
2023-03-10 17:16:07 -0800

    I commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the command line arguments.
+ * Return: 0 in case of success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}	
