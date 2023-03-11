179e50819255c4b2cc3ce966408de3edb0d1cd25
buvly <buvly.w@gmail.com>
2023-03-10 17:21:28 -0800

    I commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the command line arguments.
+ * Return: 0 in case of success.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
