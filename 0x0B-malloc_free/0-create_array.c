a844b1bccbbe0525ce008a278979251b4590a93b
buvly <buvly.w@gmail.com>
2023-03-14 00:16:19 -0700

    Commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * create_array - creats an array of chars.
@@ -10,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
